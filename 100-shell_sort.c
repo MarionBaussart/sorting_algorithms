48f3101ab7f6ab681740e76a65ab4fd9fe58d154
MarionBaussart <marion.baussart@gmail.com>
2023-10-06 14:51:25 +0000

    add main

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * shell_sort - sorts an array of integers in ascending order
@@ -11,7 +12,7 @@
 void shell_sort(int *array, size_t size)
 {
 	size_t interval = 1;
-	size_t i = interval, j = i * 3 + 1;
+	size_t i = interval, j = i * 3 + 1, k = 0;
 	int tmp;
 
 	if (!array || size < 1)
@@ -24,6 +25,7 @@ void shell_sort(int *array, size_t size)
 			tmp = array[i - 1];
 			array[i - 1] = array[j - 1];
 			array[j - 1] = tmp;
+			printf("first boucle\n");
 		}
 		i++;
 		j++;
@@ -32,10 +34,12 @@ void shell_sort(int *array, size_t size)
 
 	i = 0;
 	j = i + 1;
-	while (i < size - 1)
+	for (k = 0; k < size - 1; k++)
 	{
+		printf("2 boucle\n");
 		while (array[i] > array[j] && i > 0 && j > 0)
 		{
+			printf("3 boucle\n");
 			tmp = array[i];
 			array[i] = array[j];
 			array[j] = tmp;
@@ -43,6 +47,5 @@ void shell_sort(int *array, size_t size)
 			i--;
 			j = i + 1;
 		}
-		i++;
 	}
 }
